d94489662bb978ad9921a1dfa6db0127b7d59499
Daniel Hails <daniel@hails.info>
2019-05-27 22:55:14 +0100

    Allow custom image overtop

--- lib/info_gathering.dart
+++ lib/info_gathering.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:inkstep/widgets.dart';
 import 'package:inkstep/info_gathering/short_text_input.dart';
+import 'package:inkstep/widgets.dart';
 
+import 'boarding.dart';
 import 'info_gathering/concept_question.dart';
 
 class NewJourneyRoute extends StatefulWidget {
@@ -26,24 +27,35 @@ class _NewJourneyRouteState extends State<NewJourneyRoute> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create a new journey'),
-      ),
-      body: PageView(
-        controller: controller,
-        scrollDirection: Axis.vertical,
-        children: <Widget>[
-          nameQ,
-          conceptQ,
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          body: PageView(
+            controller: controller,
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              nameQ,
+              conceptQ,
 //          styleQ,
 //          positionQ,
 //          sizeQ,
 //          availabilityQ,
 //          depositQ,
 //          legalQ,
-        ],
-      ),
+            ],
+          ),
+        ),
+        SafeArea(
+          child: Container(
+              child: Hero(
+                tag: 'logo',
+                child: LogoWidget(),
+              ),
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.only(left: 30.0),
+              width: 100),
+        ),
+      ],
     );
   }
 }
