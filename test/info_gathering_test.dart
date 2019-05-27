90944207d00b329a7de8ccadd59d0119cdd2e8e2
Daniel Hails <daniel@hails.info>
2019-05-27 19:31:13 +0100

    Update tests to match application's current form
    Blank and empty

---
+++ test/info_gathering_test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+  testWidgets('App should start with requesting a name', (WidgetTester tester) async {
+//    await tester.pumpWidget(app);
+//
+//    expect(find.text('Register'), findsOneWidget);
+//    expect(find.text('I agree to the Terms of Services and Privacy Policy'),
+//        findsOneWidget);
+//    expect(find.byType(TextFormField), findsNWidgets(2));
+//    expect(find.byType(OutlineButton), findsOneWidget);
+//    expect(find.byType(Checkbox), findsOneWidget);
+  });
+  
+  testWidgets('Onboarding can be successfully completed with default names', (WidgetTester tester) async {
+//      await tester.pumpWidget(app);
+//      final Finder nickname = find.widgetWithText(TextFormField, 'Nickname');
+//      final Finder fullName = find.widgetWithText(TextFormField, 'Full name');
+//      final Finder submit = find.widgetWithText(OutlineButton, 'Register');
+//
+//      expect(find.text('Form submitted'), findsNothing);
+//
+//      await tester.enterText(nickname, 'Jess');
+//      await tester.enterText(fullName, 'Jess Sampson');
+//
+//      await tester.tap(submit);
+//      await tester.pump();
+//
+//      expect(find.text('Form submitted'), findsOneWidget);
+  });
+  
+  testWidgets('All required elements completed', (WidgetTester tester) async {
+//    await tester.pumpWidget(app);
+//    final Finder submit = find.widgetWithText(OutlineButton, 'Register');
+//    await tester.tap(submit);
+//    await tester.pump();
+//
+//    expect(find.text('Nickname is required'), findsOneWidget);
+//    expect(find.text('Form submitted'), findsNothing);
+  });
+
+  testWidgets('Submit disabled if not enough images selected', (WidgetTester tester) async {
+//    await tester.pumpWidget(app);
+//    final Finder submit = find.widgetWithText(OutlineButton, 'Register');
+//    final Finder tos = find.byType(Checkbox);
+//
+//    expect(tester.widget<OutlineButton>(submit).enabled, isTrue);
+//
+//    await tester.tap(tos);
+//    await tester.tap(submit);
+//    await tester.pump();
+//
+//    expect(tester.widget<OutlineButton>(submit).enabled, isFalse);
+//    expect(find.text('Form submitted'), findsNothing);
+  });
+}
