dbc4162387819b36e40427dcd300ebee66ff41bb
Matthew Malarkey <mm5917@imperial.ac.uk>
2019-05-27 10:58:51 +0100

    Initial Flutter mobile app project setup

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/widgets.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(brightness: Brightness.dark, accentColor: Colors.white),
+      home: TopTabs(),
+    );
+  }
+}
+
+class TopTabs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Inkstep.'),
+            bottom: TabBar(
+              tabs: [
+                Tab(text: "Journeys"),
+                Tab(text: "Studios"),
+                Tab(text: "Artists"),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              JourneyPage(),
+              StudiosPage(),
+              Icon(Icons.brush),
+            ],
+          ),
+        )
+    );
+  }
+}
---
+++ lib/widgets.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class JourneyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(8.0),
+        children: <Widget>[
+          JourneyCard("Journey 1"),
+          JourneyCard("Journey 2"),
+          JourneyCard("Journey 3"),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print("New journey");
+        },
+      ),
+    );
+  }
+}
+
+class JourneyCard extends StatelessWidget {
+  JourneyCard(String this.text, {double this.height=100});
+
+  final String text;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          print('Card tapped');
+        },
+        child: Container(
+          width: 300,
+          height: height,
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+}
+
+class StudiosPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextField(
+              obscureText: false,
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Search",
+              )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text("Featured"),
+          SizedBox(
+            height: 5,
+          ),
+          JourneyCard("South City Market", height: 50,),
+          const Divider(),
+
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(8.0),
+              children: <Widget>[
+                JourneyCard("Studio 1", height: 50,),
+                JourneyCard("Studio 2", height: 50,),
+                JourneyCard("Studio 3", height: 50,),
+              ],
+            ),
+          ),
+        ],
+
+      ),
+    );
+  }
+}
