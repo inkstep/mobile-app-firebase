ea3232c29518a17e02445d575c25680c1033077a
Daniel Hails <daniel@hails.info>
2019-05-27 22:54:33 +0100

    Reflow text to get better spacing

--- lib/info_gathering/short_text_input.dart
+++ lib/info_gathering/short_text_input.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import '../main.dart';
 
 class ShortTextInput extends StatelessWidget {
-  ShortTextInput(this.formController, this.labelText,
-      this.hintText, {this.height=100});
+  ShortTextInput(this.formController, this.labelText, this.hintText,
+      {this.height = 100});
 
   final PageController formController;
   final String labelText;
@@ -30,36 +30,35 @@ class ShortTextInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 100,
-      height: height,
-      decoration: BoxDecoration(
-        color: Colors.white,
-      ),
-      padding: kPadding,
-      child: Center(
-        child:
-        TextFormField(
-          autofocus: true,
-          key: Key('name'),
-          style: TextStyle(color: darkColor, fontSize: nameFontSize),
-          cursorColor: darkColor,
-          decoration: InputDecoration(
-            hintText: hintText,
-            labelText: labelText,
-            focusedBorder: underlineBorder,
-            enabledBorder: underlineBorder,
-            labelStyle: TextStyle(color: darkColor, fontSize: labelFontSize),
-            hintStyle: hintStyle,
-          ),
-          focusNode: focus,
-          textInputAction: TextInputAction.next,
-          onFieldSubmitted: (term){
-            print('NAME: $term');
-            focus.unfocus();
-            formController.animateToPage(1, duration: kDuration, curve: kCurve);
-          },
+        width: 100,
+        height: height,
+        decoration: new BoxDecoration(
+          color: Colors.white,
         ),
-      )
-    );
+        padding: kPadding,
+        child: Center(
+          child: TextFormField(
+            autofocus: true,
+            key: Key('name'),
+            style: TextStyle(color: darkColor, fontSize: nameFontSize),
+            cursorColor: darkColor,
+            decoration: InputDecoration(
+              hintText: hintText,
+              labelText: labelText,
+              focusedBorder: underlineBorder,
+              enabledBorder: underlineBorder,
+              labelStyle: TextStyle(color: darkColor, fontSize: labelFontSize),
+              hintStyle: hintStyle,
+            ),
+            focusNode: focus,
+            textInputAction: TextInputAction.next,
+            onFieldSubmitted: (term) {
+              print('NAME: $term');
+              focus.unfocus();
+              formController.animateToPage(1,
+                  duration: kDuration, curve: kCurve);
+            },
+          ),
+        ));
   }
 }
