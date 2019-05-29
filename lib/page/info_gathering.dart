d9b813b6f402bd9751495982c247636dc6619f5c
Daniel Hails <daniel@hails.info>
2019-05-29 17:37:32 +0100

    Update multi image picker to work

--- lib/page/info_gathering.dart
+++ lib/page/info_gathering.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:inkstep/component/grid_image_picker.dart';
+import 'package:flutter/services.dart';
 import 'package:inkstep/component/short_text_input.dart';
 import 'package:inkstep/logo.dart';
+import 'package:inkstep/main.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 
 class InfoScreen extends StatefulWidget {
   @override
@@ -14,12 +16,128 @@ class _InfoScreenState extends State<InfoScreen> {
     initialPage: 0,
   );
 
+  List<Asset> images = List<Asset>();
+  String _error;
+
+  int get autoScrollDuration => 500;
+
+  Widget showImage(int i, double size) {
+    Widget inner;
+    if (i < images.length) {
+      print('rerendering');
+      final Asset asset = images[i];
+      inner = AssetThumb(
+        asset: asset,
+        width: size.round(),
+        height: size.round(),
+      );
+    } else {
+      inner = Icon(
+        Icons.add,
+        size: 60.0,
+        color: baseColors['dark'],
+      );
+    }
+    return Container(
+      margin: EdgeInsets.all(8.0),
+//      decoration: BoxDecoration(
+//        color: Theme.of(context).backgroundColor,
+//      ),
+      width: size,
+      height: size,
+      child: InkWell(
+        child: inner,
+        onTap: updateAssets,
+      ),
+    );
+  }
+
+  Future<void> updateAssets() async {
+    List<Asset> resultList;
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+          maxImages: 6, selectedAssets: images);
+    } on PlatformException catch (e) {
+      setState(() {
+        _error = e.message;
+      });
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      images = resultList;
+    });
+  }
+
   // Should contain existing user flag to skip nameQ / contactQ
 
-  Widget get nameQ =>
-      ShortTextInput(controller, 'What do your friends call you?', 'Natasha');
+  Widget get nameQ => ShortTextInput(controller,
+      label: 'What do your friends call you?',
+      hint: 'Natasha',
+      duration: autoScrollDuration);
   //Widget get styleQ => ('style');
-  Widget get conceptQ => GridImagePicker();
+  Widget get conceptQ {
+    final double thumbSize = MediaQuery.of(context).size.width * 0.4;
+    return Column(
+      children: <Widget>[
+        Text(
+          "Let's get started!",
+          style: Theme.of(context)
+              .textTheme
+              .title
+              .copyWith(color: baseColors['dark']),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: Text(
+            "Choose some reference images, showing what you want. You'll get to talk about these later.",
+            style: Theme.of(context)
+                .textTheme
+                .subhead
+                .copyWith(color: baseColors['dark']),
+          ),
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              showImage(0, thumbSize),
+              showImage(1, thumbSize)
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              showImage(2, thumbSize),
+              showImage(3, thumbSize)
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              showImage(4, thumbSize),
+              showImage(5, thumbSize)
+            ]),
+        images.length > 1
+            ? RaisedButton(
+                child: Text('Ok?'),
+                onPressed: () {
+                  controller.nextPage(
+                      duration: Duration(milliseconds: autoScrollDuration),
+                      curve: Curves.ease);
+                },
+              )
+            : Container(),
+      ],
+    );
+  }
 //  Widget get positionQ => JourneyCard('position');
 //  Widget get sizeQ => JourneyCard('size');
 //  Widget get availabilityQ => JourneyCard('availability');
