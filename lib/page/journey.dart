b9267d55aa9f53bc3aa8721847fbd45021a5dc63
Daniel Hails <daniel@hails.info>
2019-05-29 13:40:13 +0100

    Rename elements to use larger cards

--- lib/page/journey.dart
+++ lib/page/journey.dart
@@ -1,101 +1,234 @@
 import 'package:flutter/material.dart';
+import 'package:inkstep/main.dart';
 import 'package:inkstep/page/info_gathering.dart';
 
-class JourneyPage extends StatelessWidget {
+class JourneyPage extends StatefulWidget {
+  @override
+  _JourneyPageState createState() => _JourneyPageState();
+}
+
+class _JourneyPageState extends State<JourneyPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
+  final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
+  PageController _pageController;
+  int _currentPageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    _pageController = PageController(initialPage: 0, viewportFraction: 0.8);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    var _isLoading = false;
+    var _length = 1;
+
+    if (!_isLoading) {
+      _controller.forward();
+    }
     return Scaffold(
-      /*body: ListView(
-        padding: const EdgeInsets.all(8.0),
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        title: Text(''),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 1.0,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            )
+          : FadeTransition(
+              opacity: _animation,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _buildHeader(context),
+                  _buildJourneyCards(_length),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 32.0),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+
+  Expanded _buildJourneyCards(int _length) {
+    return Expanded(
+      key: _backdropKey,
+      flex: 1,
+      child: NotificationListener<ScrollNotification>(
+        onNotification: (notification) {
+          if (notification is ScrollEndNotification) {
+            print('ScrollNotification = ${_pageController.page}');
+            var currentPage = _pageController.page.round().toInt();
+            if (_currentPageIndex != currentPage) {
+              setState(() => _currentPageIndex = currentPage);
+            }
+          }
+        },
+        child: PageView.builder(
+          controller: _pageController,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == _length) {
+              return AddCard();
+            } else {
+              return JourneyCard();
+            }
+          },
+          itemCount: _length + 1,
+        ),
+      ),
+    );
+  }
+
+  Container _buildHeader(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 0.0, left: 56.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          JourneyCard("Journey 1"),
-          JourneyCard("Journey 2"),
-          JourneyCard("Journey 3"),
+          // ShadowImage(),
+          Text(
+            'Welcome back,',
+            style: Theme.of(context)
+                .textTheme
+                .title
+                .copyWith(color: Colors.white.withOpacity(0.7)),
+          ),
+          Container(
+            // margin: EdgeInsets.only(top: 22.0),
+            child: Text(
+              'Natasha',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline
+                  .copyWith(color: Colors.white),
+            ),
+          ),
+          Container(height: 16.0),
+          Text(
+            'You have some journey tasks to complete',
+            style: Theme.of(context)
+                .textTheme
+                .body1
+                .copyWith(color: Colors.white.withOpacity(0.7)),
+          ),
+          Container(
+            height: 16.0,
+          )
         ],
-      ),*/
-      body: LargeJourneyCard(),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            Navigator.push<dynamic>(
-              context,
-              MaterialPageRoute<dynamic>(builder: (context) => InfoScreen()),
-            );
-          }),
+      ),
     );
   }
 }
 
-class JourneyCard extends StatelessWidget {
-  const JourneyCard(this.text, {this.height = 100});
-
-  final String text;
-  final double height;
+class AddCard extends StatelessWidget {
+  const AddCard({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final textColor = Theme.of(context).backgroundColor;
     return Card(
-      child: InkWell(
-        onTap: () {
-          print('Card tapped');
-        },
-        child: Container(
-          width: 300,
-          height: height,
-          child: Text(text),
+      margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(16.0),
+        color: Theme.of(context).cardColor,
+        child: InkWell(
+          onTap: () {
+            Navigator.push<dynamic>(context,
+                MaterialPageRoute<dynamic>(builder: (BuildContext context) {
+              return InfoScreen();
+            }));
+          },
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.add,
+                  size: 52.0,
+                  color: textColor,
+                ),
+                Container(
+                  height: 8.0,
+                ),
+                Text(
+                  'Start a new journey',
+                  style: TextStyle(color: textColor),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
 
-class ArtistProfileRow extends StatelessWidget {
+typedef JourneyGetter<T, V> = V Function(T value);
+
+class JourneyCard extends StatelessWidget {
+  JourneyCard();
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        CircleAvatar(
-          radius: 30.0,
-          backgroundImage: AssetImage('assets/ricky.png'),
-          backgroundColor: Colors.transparent,
-        ),
-        SizedBox(width: 20),
-        Expanded(
+//    var heroIds = getHeroIds(task);
+    return GestureDetector(
+      onTap: () {
+        print('Existing card tapped');
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Ricky Williams',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Spacer(
+                flex: 8,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 4.0),
+                // Should be Hero-d
+                child: Text(
+                  'Ricky',
+                  style: Theme.of(context)
+                      .textTheme
+                      .body1
+                      .copyWith(color: baseColors['gray']),
                 ),
               ),
-              Text('South City Market')
-            ],
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class LargeJourneyCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 40),
-      width: 100000,
-      height: 100000,
-      child: Card(
-        child: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              ArtistProfileRow(),
-              Expanded(
-                child: SizedBox(height: 20.0),
+              Container(
+                // Should be Hero-d
+                child: Text('Dove',
+                    style: Theme.of(context)
+                        .textTheme
+                        .title
+                        .copyWith(color: Theme.of(context).accentColor)),
               ),
+              Spacer(),
+              //Journey progression?
             ],
           ),
         ),
