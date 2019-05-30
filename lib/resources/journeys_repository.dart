4af3cb5b8d955a021eadc7ff6cab24dea5b33f55
Daniel Hails <daniel@hails.info>
2019-05-30 23:39:01 +0100

    Repository should make the request

--- lib/resources/journeys_repository.dart
+++ lib/resources/journeys_repository.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:core';
+
+import 'package:http/http.dart' as http;
 
 // TODO(DJRHails): provide local file storage as well
 class JourneysRepository {
   const JourneysRepository({
-    this.webClient = const WebClient(),
+    this.webClient,
   });
 
   final WebClient webClient;
@@ -23,7 +26,11 @@ class JourneysRepository {
 }
 
 class WebClient {
-  const WebClient([this.delay = const Duration(milliseconds: 300)]);
+  const WebClient([
+    this.delay = const Duration(milliseconds: 300),
+  ]);
+
+  final String url = 'inkstep-backend.eu-west-2.elasticbeanstalk.com';
 
   final Duration delay;
 
@@ -40,7 +47,19 @@ class WebClient {
   }
 
   Future<bool> postJourneys(List<Map<String, dynamic>> journeys) async {
-    print('trying to post');
+    for (Map<String, dynamic> journeyMap in journeys) {
+      String jsonStr = jsonEncode(journeyMap);
+      print(jsonStr);
+
+      var response = await http.put(
+          'http://inkstep-backend.eu-west-2.elasticbeanstalk.com/journey',
+          body: jsonStr,
+          headers: {'Content-Type': 'application/json'});
+      print('Status Code: ${response.statusCode}');
+      if (response.statusCode != 200) {
+        return Future.value(false);
+      }
+    }
     return Future.value(true);
   }
 }
