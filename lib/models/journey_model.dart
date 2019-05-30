5b80cdd3d34365f26ce8b086df7e6d3075b246e9
Matthew Malarkey <mm5917@imperial.ac.uk>
2019-05-30 18:11:52 +0100

    Add from json constructor to journey model

--- lib/models/journey_model.dart
+++ lib/models/journey_model.dart
@@ -1,6 +1,10 @@
 class JourneyModel {
-  JourneyModel(this.artistName, this.studioName);
-
   final String artistName;
   final String studioName;
+
+  JourneyModel(this.artistName, this.studioName);
+
+  JourneyModel.fromJson(Map<String, dynamic> json)
+      : artistName = json['artist'],
+        studioName = json['studio'];
 }
