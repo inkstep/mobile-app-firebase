f95e163ad04cbff9a0937ddad6e7ac50ed323fdb
Daniel Hails <daniel@hails.info>
2019-05-30 22:25:49 +0100

    Add remaining fields to journey

--- lib/models/journey_model.dart
+++ lib/models/journey_model.dart
@@ -1,20 +1,67 @@
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
 class Journey extends Equatable {
-  Journey(this.artistName, this.studioName)
-      : super(<dynamic>[artistName, studioName]);
+  Journey({
+    @required this.artistName,
+    this.studioName = '',
+    this.artistEmail = 'daniel@hails.info',
+    this.type = '',
+    @required this.name,
+    @required this.mentalImage,
+    @required this.size,
+    this.position = '',
+    @required this.email,
+    @required this.availability,
+    @required this.deposit,
+  }) : super(<dynamic>[
+          artistName,
+          studioName,
+          artistEmail,
+          type,
+          name,
+          mentalImage,
+          size,
+          position,
+          email,
+          availability,
+          deposit
+        ]);
 
   Journey.fromJson(Map<String, dynamic> json)
-      : artistName = json['artist'],
-        studioName = json['studio'];
+      : artistName = json['artistName'],
+        studioName = '',
+        artistEmail = json['artistEmail'],
+        name = json['userName'],
+        type = json['tattoo'],
+        mentalImage = json['desc'],
+        size = json['size'],
+        position = json['pos'],
+        email = '',
+        availability = '',
+        deposit = '';
 
   final String artistName;
   final String studioName;
+  final String artistEmail;
+  final String name;
+  final String mentalImage;
+  final String size;
+  final String position;
+  final String email;
+  final String availability;
+  final String deposit;
+  final String type;
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
-      'artist': artistName,
-      'studio': studioName,
+      'artistName': artistName,
+      'artistEmail': artistEmail,
+      'userName': name,
+      'tattoo': type,
+      'desc': mentalImage,
+      'size': size,
+      'pos': position,
     };
   }
 }
