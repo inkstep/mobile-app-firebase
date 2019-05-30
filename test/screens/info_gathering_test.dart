feeb6cacee4d1f0ff613e24566751cc8d477b1b3
Daniel Hails <daniel@hails.info>
2019-05-30 17:43:52 +0100

    Migrate tests to be inside screen folder

--- test/screens/info_gathering_test.dart
+++ test/screens/info_gathering_test.dart
@@ -1,19 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:inkstep/di/service_locator.dart';
+import 'package:inkstep/ui/pages/journey_page.dart';
 
 void main() {
-  testWidgets('App should start with requesting a name', (WidgetTester tester) async {
-//    await tester.pumpWidget(app);
-//
-//    expect(find.text('Register'), findsOneWidget);
-//    expect(find.text('I agree to the Terms of Services and Privacy Policy'),
-//        findsOneWidget);
-//    expect(find.byType(TextFormField), findsNWidgets(2));
-//    expect(find.byType(OutlineButton), findsOneWidget);
-//    expect(find.byType(Checkbox), findsOneWidget);
-  });
-  
-  testWidgets('Onboarding can be successfully completed with default names', (WidgetTester tester) async {
+  group('Having New Journey Screen', () {
+    setUp(() {
+      setup();
+    });
+
+    testWidgets(
+        'App should start with requesting a name if not already provided',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(JourneyPage());
+
+      expect(find.text('Register'), findsOneWidget);
+      expect(find.byType(TextFormField), findsNWidgets(1));
+//      expect(find.byType(OutlineButton), findsOneWidget);
+//      expect(find.byType(Checkbox), findsOneWidget);
+    });
+
+    testWidgets('Onboarding can be successfully completed with default names',
+        (WidgetTester tester) async {
 //      await tester.pumpWidget(app);
 //      final Finder nickname = find.widgetWithText(TextFormField, 'Nickname');
 //      final Finder fullName = find.widgetWithText(TextFormField, 'Full name');
@@ -28,9 +36,9 @@ void main() {
 //      await tester.pump();
 //
 //      expect(find.text('Form submitted'), findsOneWidget);
-  });
-  
-  testWidgets('All required elements completed', (WidgetTester tester) async {
+    });
+
+    testWidgets('All required elements completed', (WidgetTester tester) async {
 //    await tester.pumpWidget(app);
 //    final Finder submit = find.widgetWithText(OutlineButton, 'Register');
 //    await tester.tap(submit);
@@ -38,9 +46,10 @@ void main() {
 //
 //    expect(find.text('Nickname is required'), findsOneWidget);
 //    expect(find.text('Form submitted'), findsNothing);
-  });
+    });
 
-  testWidgets('Submit disabled if not enough images selected', (WidgetTester tester) async {
+    testWidgets('Submit disabled if not enough images selected',
+        (WidgetTester tester) async {
 //    await tester.pumpWidget(app);
 //    final Finder submit = find.widgetWithText(OutlineButton, 'Register');
 //    final Finder tos = find.byType(Checkbox);
@@ -53,5 +62,6 @@ void main() {
 //
 //    expect(tester.widget<OutlineButton>(submit).enabled, isFalse);
 //    expect(find.text('Form submitted'), findsNothing);
+    });
   });
 }
