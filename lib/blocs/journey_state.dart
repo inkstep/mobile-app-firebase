833abf3b193474c1f9f8fe6f7844c168651115d1
Daniel Hails <daniel@hails.info>
2019-05-30 21:50:42 +0100

    Add in Journey State

---
+++ lib/blocs/journey_state.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+import 'package:inkstep/models/journey_model.dart';
+
+abstract class JourneyState extends Equatable {
+  JourneyState([List<dynamic> props = const <dynamic>[]]) : super(props);
+}
+
+class JourneyUninitialised extends JourneyState {
+  @override
+  String toString() => 'JourneyUninitialised';
+}
+
+class JourneyError extends JourneyState {
+  @override
+  String toString() => 'JourneyError';
+}
+
+class JourneyLoaded extends JourneyState {
+  JourneyLoaded({
+    this.journeys,
+  }) : super(<dynamic>[journeys]);
+
+  final List<Journey> journeys;
+
+  @override
+  String toString() => 'JourneyLoaded { journeys: ${journeys.length}';
+}
