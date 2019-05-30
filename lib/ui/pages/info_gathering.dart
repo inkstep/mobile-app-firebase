f23b6fa3091d532b1c2426ce39f7e210218d5b80
Daniel Hails <daniel@hails.info>
2019-05-30 11:09:40 +0100

    Rewrite grid to use multi-image-picker

--- lib/ui/pages/info_gathering.dart
+++ lib/ui/pages/info_gathering.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:inkstep/ui/components/grid_image_picker.dart';
+import 'package:flutter/services.dart';
+import 'package:inkstep/main.dart';
 import 'package:inkstep/ui/components/logo.dart';
 import 'package:inkstep/ui/components/short_text_input.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 
 class InfoScreen extends StatefulWidget {
   @override
@@ -13,50 +15,148 @@ class _InfoScreenState extends State<InfoScreen> {
   final PageController controller = PageController(
     initialPage: 0,
   );
+  List<Asset> images = <Asset>[];
+  // ignore: unused_field
+  String _error;
 
-  // Should contain existing user flag to skip nameQ / contactQ
+  int get autoScrollDuration => 500;
 
-  Widget get nameQ =>
-      ShortTextInput(controller, 'What do your friends call you?', 'Natasha');
-  //Widget get styleQ => ('style');
-  Widget get conceptQ => TattooConcept();
-//  Widget get positionQ => JourneyCard('position');
-//  Widget get sizeQ => JourneyCard('size');
-//  Widget get availabilityQ => JourneyCard('availability');
-//  Widget get depositQ => JourneyCard('deposit');
-//  Widget get legalQ => JourneyCard('legal');
+  Widget buildImageThumbnail(int i, double size) {
+    Widget inner;
+    if (i < images.length) {
+      final Asset asset = images[i];
+      inner = AssetThumb(
+        asset: asset,
+        width: size.round(),
+        height: size.round(),
+      );
+    } else {
+      inner = Icon(
+        Icons.add,
+        size: 60.0,
+        color: baseColors['dark'],
+      );
+    }
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      width: size,
+      height: size,
+      child: InkWell(
+        child: inner,
+        onTap: updateAssets,
+      ),
+    );
+  }
+
+  Future<void> updateAssets() async {
+    List<Asset> resultList;
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+          maxImages: 6, selectedAssets: images);
+    } on PlatformException catch (e) {
+      setState(() {
+        _error = e.message;
+      });
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      images = resultList;
+    });
+  }
+
+  Widget get nameQ => ShortTextInput(
+        controller,
+        label: 'What do your friends call you?',
+        hint: 'Natasha',
+        duration: autoScrollDuration,
+      );
+  Widget get conceptQ {
+    final double thumbSize = MediaQuery.of(context).size.width * 0.4;
+    return Column(
+      children: <Widget>[
+        Text(
+          "Let's get started!",
+          style: Theme.of(context)
+              .textTheme
+              .title
+              .copyWith(color: baseColors['dark']),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: Text(
+            "Choose some reference images, showing what you want. You'll get to talk about these later.",
+            style: Theme.of(context)
+                .textTheme
+                .subhead
+                .copyWith(color: baseColors['dark']),
+          ),
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              buildImageThumbnail(0, thumbSize),
+              buildImageThumbnail(1, thumbSize)
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              buildImageThumbnail(2, thumbSize),
+              buildImageThumbnail(3, thumbSize)
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              buildImageThumbnail(4, thumbSize),
+              buildImageThumbnail(5, thumbSize)
+            ]),
+        images.length > 1
+            ? RaisedButton(
+                child: Text('Ok?'),
+                onPressed: () {
+                  controller.nextPage(
+                      duration: Duration(milliseconds: autoScrollDuration),
+                      curve: Curves.ease);
+                },
+              )
+            : Container(),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Scaffold(
-          body: PageView(
-            controller: controller,
-            scrollDirection: Axis.vertical,
-            children: <Widget>[
-              nameQ,
-              conceptQ,
-//          styleQ,
-//          positionQ,
-//          sizeQ,
-//          availabilityQ,
-//          depositQ,
-//          legalQ,
-            ],
-          ),
+    return Scaffold(
+      backgroundColor: Theme.of(context).cardColor,
+      appBar: AppBar(
+        title: Hero(
+          tag: 'logo',
+          child: LogoWidget(),
         ),
-        SafeArea(
-          child: Container(
-              child: Hero(
-                tag: 'logo',
-                child: LogoWidget(),
-              ),
-              alignment: Alignment.topLeft,
-              padding: EdgeInsets.only(left: 30.0),
-              width: 100),
-        ),
-      ],
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        iconTheme: Theme.of(context)
+            .iconTheme
+            .copyWith(color: Theme.of(context).backgroundColor),
+      ),
+      body: PageView(
+        controller: controller,
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          nameQ,
+          conceptQ,
+        ],
+      ),
     );
   }
 }
