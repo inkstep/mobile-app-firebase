05466e97818105834d4a70d0c098bc2f66dcb97a
Michael Black <michaelblack@Michaels-MacBook-Air.local>
2019-05-30 22:24:58 +0100

    Add page to enter desired position on body of tattoo

---
+++ lib/ui/components/location_question.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:inkstep/ui/components/short_text_input.dart';
+
+class PositionQuestion extends StatelessWidget {
+  const PositionQuestion({Key key, this.controller,
+    this.autoScrollDuration, this.func}) : super(key: key);
+
+  final void Function(String) func;
+  final PageController controller;
+  final int autoScrollDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShortTextInput(
+      controller,
+      label: 'Where on your body do you want the tattoo',
+      hint: 'Lower left forearm',
+      duration: autoScrollDuration,
+      func: func,
+    );
+  }
+}
