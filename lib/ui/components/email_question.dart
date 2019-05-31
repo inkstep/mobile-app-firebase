c2d9d00d7e84aa376af0ccd1f22bf7d5ca25cef2
Daniel Hails <daniel@hails.info>
2019-05-31 14:29:31 +0100

    Rename controller and make named parameters

--- lib/ui/components/email_question.dart
+++ lib/ui/components/email_question.dart
@@ -2,22 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:inkstep/ui/components/short_text_input.dart';
 
 class EmailQuestion extends StatelessWidget {
-  const EmailQuestion({Key key, this.controller,
-    this.autoScrollDuration, this.func}) : super(key: key);
+  const EmailQuestion(
+      {Key key, this.controller, this.autoScrollDuration, this.submitCallback})
+      : super(key: key);
 
-  final void Function(String) func;
+  final void Function(String) submitCallback;
   final PageController controller;
   final int autoScrollDuration;
 
   @override
   Widget build(BuildContext context) {
     return ShortTextInput(
-      controller,
-      Key('email'),
+      controller: controller,
       label: 'What is your email address?',
       hint: 'example@inkstep.com',
-      duration: autoScrollDuration,
-      func: func,
+      callback: submitCallback,
     );
   }
 }
