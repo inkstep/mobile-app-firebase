1b0ae8ca37a54baaeb8a2763564b0c8d9509bdb5
Matthew Malarkey <mm5917@imperial.ac.uk>
2019-06-02 13:57:31 +0100

    Add commented out failing tests for journey bloc

--- test/blocs/journey_bloc.dart
+++ test/blocs/journey_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:inkstep/blocs/journey_bloc.dart';
+import 'package:inkstep/blocs/journey_event.dart';
 import 'package:inkstep/blocs/journey_state.dart';
+import 'package:inkstep/models/journey_model.dart';
 import 'package:inkstep/resources/journeys_repository.dart';
 import 'package:mockito/mockito.dart';
 
@@ -17,5 +19,27 @@ void main() {
     test('initial state is unititialised', () {
       expect(journeyBloc.initialState, JourneyUninitialised());
     });
+
+    /*test('add journey event with missing journey does nothing', () {
+      expectLater(journeyBloc.state, neverEmits(anything));
+//      expectLater(journeyBloc.state, emits(journeyBloc.initialState));
+
+      journeyBloc.dispatch(AddJourney(null));
+    });
+
+    test('add journey event should emit the the current state plus the additional journey', () {
+      Journey testJourney = Journey(
+          availability: 'availability',
+          deposit: 'deposit',
+          size: 'size',
+          artistName: 'artistName',
+          name: 'name',
+          position: 'position',
+          mentalImage: 'mentalImage',
+          email: 'email');
+      expectLater(journeyBloc.state, emits(JourneyLoaded(journeys:  [testJourney])));
+
+      journeyBloc.dispatch(AddJourney(testJourney));
+    });*/
   });
 }
