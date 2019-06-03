0457c64dcbf29acbbca56a801d16a1cb9f3df578
Daniel Hails <daniel@hails.info>
2019-06-03 17:45:38 +0100

    Add unused_field so we remember to add in error handling for images

--- lib/ui/pages/new_screen.dart
+++ lib/ui/pages/new_screen.dart
@@ -11,6 +11,7 @@ import 'package:inkstep/blocs/journeys_event.dart';
 import 'package:inkstep/di/service_locator.dart';
 import 'package:inkstep/models/journey_model.dart';
 import 'package:inkstep/ui/components/binary_input.dart';
+import 'package:inkstep/ui/components/bold_call_to_action.dart';
 import 'package:inkstep/ui/components/dropdown_menu.dart';
 import 'package:inkstep/ui/components/form_element_builder.dart';
 import 'package:inkstep/ui/components/logo.dart';
@@ -51,6 +52,7 @@ class _NewScreenState extends State<NewScreen> {
   final TextEditingController emailController = TextEditingController();
 
   List<Asset> inspirationImages = <Asset>[];
+  // ignore: unused_field
   String _imagesError;
 
   final _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -122,17 +124,13 @@ class _NewScreenState extends State<NewScreen> {
               label: 'What is your email address?',
               hint: 'example@inkstep.com',
             ),
-            RaisedButton(
-              onPressed: _finishForm,
-              elevation: 15.0,
-              color: baseColors['dark'],
-              textColor: baseColors['light'],
-              padding: EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-              child: Text(
-                "Let's contact your artist!",
-                style: TextStyle(fontSize: 20.0, fontFamily: 'Signika'),
-              ),
+            ContactForm(
+              formData: formData,
+              nameController: nameController,
+              descController: descController,
+              emailController: emailController,
+              sizeController: sizeController,
+              scaffoldKey: _scaffoldKey,
             )
           ],
         ),
@@ -244,7 +242,7 @@ class _NewScreenState extends State<NewScreen> {
       inner = Icon(
         Icons.add,
         size: 60.0,
-        color: baseColors['dark'],
+        color: Theme.of(context).accentIconTheme.color,
       );
     }
     return Container(
@@ -280,60 +278,157 @@ class _NewScreenState extends State<NewScreen> {
       inspirationImages = resultList;
     });
   }
+}
 
-  void _finishForm() {
-    bool missingParams = false;
+class ContactForm extends StatelessWidget {
+  const ContactForm({
+    Key key,
+    @required this.formData,
+    @required this.nameController,
+    @required this.descController,
+    @required this.emailController,
+    @required this.sizeController,
+    @required GlobalKey<ScaffoldState> scaffoldKey,
+  })  : _scaffoldKey = scaffoldKey,
+        super(key: key);
 
+  final Map<String, String> formData;
+  final TextEditingController nameController;
+  final TextEditingController descController;
+  final TextEditingController emailController;
+  final TextEditingController sizeController;
+  final GlobalKey<ScaffoldState> _scaffoldKey;
+
+  @override
+  Widget build(BuildContext context) {
     formData['name'] = nameController.text;
     formData['mentalImage'] = descController.text;
     formData['email'] = emailController.text;
     formData['size'] = sizeController.text;
 
-    String missing = '';
+    return Container(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Flexible(
+            flex: 2,
+            child: Text(
+              'Check your details!',
+              style: Theme.of(context).accentTextTheme.title,
+            )),
+        Spacer(flex: 1),
+        Expanded(
+          flex: 10,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      getLabel(context, 'Name', formData, 'name'),
+                      getLabel(context, 'Description', formData, 'mentalImage'),
+                      getLabel(context, 'Position', formData, 'position'),
+                      getLabel(context, 'Size', formData, 'size'),
+                      getLabel(context, 'Availability', formData, 'availability'),
+                      getLabel(context, 'Deposit', formData, 'deposit'),
+                      getLabel(context, 'Email', formData, 'email'),
+                    ],
+                  )),
+              Expanded(
+                  flex: 5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      getData(context, formData, 'name'),
+                      getData(context, formData, 'mentalImage'),
+                      getData(context, formData, 'position'),
+                      getData(context, formData, 'size'),
+                      getData(context, formData, 'availability'),
+                      getData(context, formData, 'deposit'),
+                      getData(context, formData, 'email'),
+                    ],
+                  ))
+            ],
+          ),
+        ),
+        Spacer(flex: 1),
+        Expanded(
+            flex: 2,
+            child: missingData(formData)
+                ? Text(
+                    'Please go back and fill in your missing data!',
+                    style: Theme.of(context).accentTextTheme.subtitle,
+                    textAlign: TextAlign.center,
+                  )
+                : BoldCallToAction(
+                    label: 'Contact Artist!',
+                    color: Theme.of(context).backgroundColor,
+                    textColor: Theme.of(context).cardColor,
+                    onTap: () {
+                      final JourneysBloc journeyBloc = BlocProvider.of<JourneysBloc>(context);
+                      journeyBloc.dispatch(
+                        AddJourney(
+                          Journey(
+                            // TODO(DJRHails): Don't hardcode these
+                            artistName: 'Ricky',
+                            studioName: 'South City Market',
+                            name: formData['name'],
+                            size: formData['size'],
+                            email: formData['email'],
+                            availability: formData['availability'],
+                            deposit: formData['deposit'],
+                            mentalImage: formData['mentalImage'],
+                            position: formData['position'],
+                          ),
+                        ),
+                      );
+                      final ScreenNavigator nav = sl.get<ScreenNavigator>();
+                      nav.openJourneyScreen(context);
+                    },
+                  )),
+        Spacer(flex: 1),
+      ],
+    ));
+  }
 
+  bool missingData(Map formData) {
     for (var key in formData.keys) {
       if (formData[key] == '') {
-        missingParams = true;
-
-        if (missing == '') {
-          missing = key;
-        } else {
-          missing += ', ' + key;
-        }
+        return true;
       }
     }
 
-    if (missingParams) {
-      final snackBar = SnackBar(
-        content: Text(
-          'You still need to provide us with ' + missing,
-          style: Theme.of(context).textTheme.subtitle,
-        ),
-        backgroundColor: Theme.of(context).backgroundColor,
-      );
+    return false;
+  }
 
-      _scaffoldKey.currentState.showSnackBar(snackBar);
+  Widget getData(BuildContext context, Map formData, String param) {
+    String data;
+
+    if (formData[param] == '') {
+      data = 'MISSING';
     } else {
-      final JourneysBloc journeyBloc = BlocProvider.of<JourneysBloc>(context);
-      journeyBloc.dispatch(
-        AddJourney(
-          Journey(
-            // TODO(DJRHails): Don't hardcode these
-            artistName: 'Ricky',
-            studioName: 'South City Market',
-            name: formData['name'],
-            size: formData['size'],
-            email: formData['email'],
-            availability: formData['availability'],
-            deposit: formData['deposit'],
-            mentalImage: formData['mentalImage'],
-            position: formData['position'],
-          ),
-        ),
-      );
-      final ScreenNavigator nav = sl.get<ScreenNavigator>();
-      nav.openJourneyScreen(context);
+      data = formData[param];
     }
+
+    return Expanded(
+      flex: 1,
+      child: AutoSizeText(data, style: Theme.of(context).accentTextTheme.body1),
+    );
+  }
+
+  Widget getLabel(BuildContext context, String dataLabel, Map formData, String param) {
+    final TextStyle style = formData[param] == ''
+        ? Theme.of(context).primaryTextTheme.subtitle
+        : Theme.of(context).accentTextTheme.subtitle;
+
+    return Expanded(
+      flex: 1,
+      child: Text(
+        dataLabel + ': ',
+        style: style,
+      ),
+    );
   }
 }
 
