253288998d59e3993d4338e5113eb908f1a4d68d
Daniel Hails <daniel@hails.info>
2019-06-04 12:32:03 +0100

    Abstract out url and get journeys

--- lib/resources/web_client.dart
+++ lib/resources/web_client.dart
@@ -6,25 +6,25 @@ class WebClient {
   const WebClient([this.delay = const Duration(milliseconds: 300)]);
 
   final Duration delay;
+  String get url => 'http://inkstep-backend.eu-west-2.elasticbeanstalk.com';
+  String get journeyEndpoint => '/journey';
 
   Future<List<Map<String, dynamic>>> loadJourneys() async {
-    return Future.delayed(
-      delay,
-      () => [],
-    );
+    final http.Response response = await http.get('$url$journeyEndpoint');
+
+    print('Response(${response.statusCode}): ${response.reasonPhrase}');
+
+    return [jsonDecode(response.body)];
   }
 
   Future<bool> saveJourneys(List<Map<String, dynamic>> journeys) async {
     for (Map<String, dynamic> journeyMap in journeys) {
       final String jsonStr = jsonEncode(journeyMap);
-      print(jsonStr);
-      print('About to send');
+      print('Saving Journey: $jsonStr');
 
-      final http.Response response = await http.put(
-          'http://inkstep-backend.eu-west-2.elasticbeanstalk.com/journey',
-          body: jsonStr,
-          headers: {'Content-Type': 'application/json'});
-      print('Status Code: ${response.statusCode}');
+      final http.Response response = await http.put('$url$journeyEndpoint',
+          body: jsonStr, headers: {'Content-Type': 'application/json'});
+      print('Response(${response.statusCode}): ${response.reasonPhrase}');
       if (response.statusCode != 200) {
         return Future.value(false);
       }
