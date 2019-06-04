47f292ca501295d9aa98a584bb95d92c231b6abc
Daniel Hails <daniel@hails.info>
2019-06-04 10:11:27 +0100

    Add position picker form element

---
+++ lib/ui/pages/new/position_picker_form_element.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:inkstep/ui/components/dropdown_menu.dart';
+import 'package:inkstep/ui/components/form_element_builder.dart';
+import 'package:inkstep/ui/components/short_text_input.dart';
+
+class PositionPickerFormElement extends StatefulWidget {
+  const PositionPickerFormElement({Key key, @required this.controller, @required this.formData})
+      : super(key: key);
+
+  final PageController controller;
+  final Map formData;
+
+  @override
+  State<StatefulWidget> createState() => _PositionPickerFormElementState(controller, formData);
+}
+
+class _PositionPickerFormElementState extends State<StatefulWidget> {
+  _PositionPickerFormElementState(this.controller, this.formData) {
+    positions = {
+      'Leg': ['Lower Leg', 'Calf'],
+      'Arm': ['Inner Wrist', 'Inner Arm', 'Biceps', 'Upper Arm', 'Side'],
+      'Chest': ['Full Front Chest', 'Pec', 'Ribs'],
+      'Foot': ['Ankle'],
+      'Head': ['Inner Ear', 'Behind Ear'],
+      'Hand': ['Fingers'],
+      'Back': ['Back', 'Shoulders', 'Neck'],
+      'Other': [],
+    };
+  }
+
+  final PageController controller;
+  final Map formData;
+
+  String generalPos;
+
+  Map<String, List<String>> positions;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormElementBuilder(
+      builder: (context, focus, onSubmitCallback) {
+        return Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Flexible(
+                  flex: generalPos == 'Other' ? 10 : 4,
+                  child: Text(
+                    'Where would you like your tattoo? (Arm, leg,'
+                    ' etc)',
+                    style: Theme.of(context).accentTextTheme.title,
+                    textAlign: TextAlign.center,
+                  )),
+              Spacer(flex: 1),
+              Flexible(
+                flex: generalPos == 'Other' ? 10 : 3,
+                child: DropdownMenu(
+                  hintText: generalPos == null ? 'General Area' : generalPos,
+                  callback: (value) {
+                    setState(() {
+                      generalPos = value;
+                      formData['position'] = null;
+                    });
+                  },
+                  items: positions.keys.toList(),
+                ),
+              ),
+              Spacer(flex: 1),
+              generalPos == 'Other'
+                  ? Spacer(flex: 1)
+                  : Flexible(
+                      flex: 2,
+                      child: Text(
+                        'Specifics...',
+                        style: Theme.of(context).accentTextTheme.title,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+              Spacer(flex: 1),
+              Flexible(
+                flex: generalPos == 'Other' ? 30 : 4,
+                child: Container(
+                  child: generalPos == 'Other'
+                      ? ShortTextInput(
+                          controller: null,
+                          label: 'Specific Area',
+                          hint: '...',
+                          maxLength: 20,
+                          callback: (term) {},
+                        )
+                      : DropdownMenu(
+                          hintText:
+                              formData['position'] == null ? 'Specifc Area' : formData['position'],
+                          callback: onSubmitCallback,
+                          items: generalPos == null ? [] : positions[generalPos],
+                        ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+      onSubmitCallback: (value) {
+        setState(() {
+          formData['position'] = value;
+        });
+      },
+      controller: controller,
+    );
+  }
+}
