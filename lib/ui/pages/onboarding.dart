8d408f68fd9aea4c96d91c4622d827db1d6e65f7
Daniel Hails <daniel@hails.info>
2019-06-14 00:19:50 +0100

    Relayout onboarding to include logo

--- lib/ui/pages/onboarding.dart
+++ lib/ui/pages/onboarding.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:inkstep/di/service_locator.dart';
-import 'package:inkstep/main.dart';
 import 'package:inkstep/ui/components/bold_call_to_action.dart';
 import 'package:inkstep/ui/components/logo.dart';
 import 'package:inkstep/ui/components/text_button.dart';
@@ -14,54 +13,9 @@ class Onboarding extends StatefulWidget {
 class _OnboardingState extends State<Onboarding> with TickerProviderStateMixin {
   _OnboardingState();
 
-  Widget _sub(String text) {
-    return Center(
-      child: Text(
-        text,
-        style: Theme.of(context).textTheme.subhead,
-      ),
-    );
-  }
-
-  Widget _header(String text) {
-    return Text(
-      text,
-      style: TextStyle(
-          color: baseColors['light'],
-          fontSize: 40.0,
-          fontFamily: 'Signika',
-          fontWeight: FontWeight.w600),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     const EdgeInsets topPadding = EdgeInsets.only(top: 120.0);
-    final top = Padding(
-      padding: topPadding,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Logo(),
-                  _header('Tattoos done right'),
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  _sub("We're here to make first time clients sound like pros"),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
 
     const EdgeInsets buttonPadding = EdgeInsets.only(top: 16.0);
     final ScreenNavigator nav = sl.get<ScreenNavigator>();
@@ -97,9 +51,42 @@ class _OnboardingState extends State<Onboarding> with TickerProviderStateMixin {
         painter: InkSplash(Theme.of(context).backgroundColor, 0.9),
         child: Container(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              top,
+              Spacer(flex: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Logo(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'inkstep.',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline
+                          .copyWith(fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Text(
+                'Tattoos done right',
+                style: Theme.of(context).textTheme.headline,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
+                child: Text(
+                  "We're here to make you feel like a tattoo pro.",
+                  style: Theme.of(context).textTheme.title,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Spacer(
+                flex: 8,
+              ),
               bottom,
             ],
           ),
