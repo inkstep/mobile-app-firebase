dc7ec3a51f27b9fbdaab799e759db9b0198f4d6a
Daniel Hails <daniel@hails.info>
2019-06-14 00:02:03 +0100

    Update onboarding temporarily

--- lib/ui/pages/onboarding.dart
+++ lib/ui/pages/onboarding.dart
@@ -15,10 +15,11 @@ class _OnboardingState extends State<Onboarding> with TickerProviderStateMixin {
   _OnboardingState();
 
   Widget _sub(String text) {
-    return Text(
-      text,
-      style: TextStyle(
-          color: Colors.grey, fontSize: 25.0, fontFamily: 'Signika', fontWeight: FontWeight.w100),
+    return Center(
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.subhead,
+      ),
     );
   }
 
@@ -46,16 +47,14 @@ class _OnboardingState extends State<Onboarding> with TickerProviderStateMixin {
               child: Column(
                 children: <Widget>[
                   Logo(),
-                  _header('Hi there,'),
-                  _header("We're here to help"),
+                  _header('Tattoos done right'),
                 ],
               ),
             ),
             Container(
               child: Column(
                 children: <Widget>[
-                  _sub('Every step'),
-                  _sub('of the way'),
+                  _sub("We're here to make first time clients sound like pros"),
                 ],
               ),
             ),
@@ -84,7 +83,8 @@ class _OnboardingState extends State<Onboarding> with TickerProviderStateMixin {
             onTap: () {
               nav.openLoginScreen(context);
             },
-            label: "I'M ON A NEW DEVICE",
+            label: "I'VE BEEN HERE BEFORE",
+            color: Theme.of(context).backgroundColor,
           ),
           Padding(padding: buttonPadding),
         ],
@@ -92,21 +92,101 @@ class _OnboardingState extends State<Onboarding> with TickerProviderStateMixin {
     );
 
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/inksplot.png'),
-            fit: BoxFit.cover,
+      backgroundColor: Colors.white,
+      body: CustomPaint(
+        painter: InkSplash(Theme.of(context).backgroundColor, 0.9),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              top,
+              bottom,
+            ],
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            top,
-            bottom,
-          ],
         ),
       ),
     );
+  }
+}
+
+class InkSplash extends CustomPainter {
+  InkSplash(this.color, this.ratio);
+
+  final Color color;
+  final double ratio;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path = Path();
+    final Paint paint = Paint();
+
+    final double height = size.height * ratio;
+
+    path.lineTo(0, height * 0.7);
+    path.quadraticBezierTo(
+      size.width * 0.10,
+      height * 0.7,
+      size.width * 0.15,
+      height * 0.75,
+    );
+    path.quadraticBezierTo(
+      size.width * 0.20,
+      height * 0.8,
+      size.width * 0.25,
+      height * 0.7,
+    );
+    path.quadraticBezierTo(
+      size.width * 0.40,
+      height * 0.40,
+      size.width * 0.50,
+      height * 0.7,
+    );
+    path.quadraticBezierTo(
+      size.width * 0.60,
+      height * 0.85,
+      size.width * 0.65,
+      height * 0.65,
+    );
+    path.quadraticBezierTo(
+      size.width * 0.70,
+      height * 0.8,
+      size.width,
+      0,
+    );
+    path.close();
+
+    paint.color = Colors.black26;
+    canvas.drawPath(path, paint);
+
+    path = Path();
+    path.lineTo(0, height * 0.50);
+    path.quadraticBezierTo(size.width * 0.10, height * 0.80, size.width * 0.15, height * 0.60);
+    path.quadraticBezierTo(size.width * 0.20, height * 0.45, size.width * 0.27, height * 0.60);
+    path.quadraticBezierTo(size.width * 0.45, height, size.width * 0.50, height * 0.80);
+    path.quadraticBezierTo(size.width * 0.55, height * 0.45, size.width * 0.8, height * 0.7);
+    path.quadraticBezierTo(size.width * 0.95, height * 0.8, size.width, height * 0.60);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    paint.color = Colors.black54;
+
+    canvas.drawPath(path, paint);
+
+    path = Path();
+    path.lineTo(0, height * 0.7);
+    path.quadraticBezierTo(size.width * 0.10, height * 0.55, size.width * 0.22, height * 0.7);
+    path.quadraticBezierTo(size.width * 0.30, height * 0.8, size.width * 0.40, height * 0.7);
+    path.quadraticBezierTo(size.width * 0.52, height * 0.50, size.width * 0.65, height * 0.7);
+    path.quadraticBezierTo(size.width * 0.75, height * 0.85, size.width, height * 0.60);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    paint.color = color;
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
   }
 }
