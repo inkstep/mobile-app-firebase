706cd6ec45aa16a54f73b0ab10a56168e2c93baa
Daniel Hails <daniel@hails.info>
2019-06-13 16:32:54 +0100

    User entity with token

--- lib/models/user_entity.dart
+++ lib/models/user_entity.dart
@@ -2,15 +2,24 @@ import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 class UserEntity extends Equatable {
-  UserEntity({@required this.name, @required this.email}) : super(<dynamic>[name, email]);
+  UserEntity({
+    @required this.name,
+    @required this.email,
+    @required this.token,
+  }) : super(<dynamic>[name, email, token]);
 
   final String name;
   final String email;
+  final String token;
 
   Map<String, dynamic> toJson() {
-    return <String, dynamic>{'user_name': name, 'user_email': email};
+    return <String, dynamic>{
+      'user_name': name,
+      'user_email': email,
+      'token': token,
+    };
   }
 
   @override
-  String toString() => 'User { name: $name, email: $email }';
+  String toString() => 'User { name: $name, email: $email, token: $token}';
 }
