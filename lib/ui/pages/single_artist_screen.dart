237f5c88f637c1e6a6b13cc366adcfeceac75599
Matthew Malarkey <mm5917@ic.ac.uk>
2020-02-11 15:16:11 +0000

    Add callback function to add a subscription to updates from an artist, build subscription button with initial value from firebase

--- lib/ui/pages/single_artist_screen.dart
+++ lib/ui/pages/single_artist_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:inkstep/di/service_locator.dart';
 import 'package:inkstep/models/artist.dart';
 import 'package:inkstep/ui/components/platform_switch.dart';
@@ -72,16 +75,48 @@ class SingleArtistScreen extends StatelessWidget {
                     padding: const EdgeInsets.only(top: 40),
                     child: Text('Subscribe to updates', style: subheading),
                   ),
-                  PlatformSwitch(
-                    callback: (_value) {
-                      print('Update subsrciption preferences!');
-                      // Update whether or not this user should get notifications for this artist
-                      // Firestore.instance.collection('artist_subs').add(
-                      //    <String, dynamic>{
-                      //     'auth_uid': '',
-                      //     'artistId': '',
-                      //   },
-                      // );
+
+                  // TODO(mm): messy, need to use persistent state model to get this stuff
+                  FutureBuilder(
+                    future: FirebaseAuth.instance.signInAnonymously(),
+                    builder: (BuildContext context, AsyncSnapshot auth) {
+                      return StreamBuilder<QuerySnapshot>(
+                        stream: Firestore.instance
+                            .collection('artist_subs')
+                            .where('auth_uid', isEqualTo: auth.hasData ? auth.data.user.uid : '-1')
+                            .where('artistId', isEqualTo: artist.artistId)
+                            .snapshots(),
+                        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                          if (snapshot.hasData) {
+                            return PlatformSwitch(
+                              initialValue: snapshot.data.documents.isNotEmpty,
+                              callback: (_value) {
+                                // Update whether or not this user should get notifications for this artist
+                                if (_value) {
+                                  Firestore.instance.collection('artist_subs').add(
+                                    <String, dynamic>{
+                                      'auth_uid': auth.data.user.uid,
+                                      'artistId': artist.artistId,
+                                    },
+                                  );
+                                } else {
+                                  // TODO(mm): delete subscription document if present
+                                  Stream<QuerySnapshot> subs = Firestore.instance
+                                      .collection('artist_subs')
+                                      .where('auth_uid', isEqualTo: auth.data.user.uid)
+                                      .where('artistId', isEqualTo: artist.artistId)
+                                      .snapshots();
+                                  // subs.forEach((doc) => doc.documents[0].);
+                                }
+                              },
+                            );
+                          }
+                          return SpinKitChasingDots(
+                            color: Theme.of(context).cardColor,
+                            size: 50.0,
+                          );
+                        },
+                      );
                     },
                   ),
                 ],
