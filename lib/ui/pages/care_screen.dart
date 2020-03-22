ab4070f59d1bc1dfefe5edbad2d5f77a65cbbd0e
Matthew Malarkey <mm5917@ic.ac.uk>
2020-03-22 16:56:58 +0000

    Move non-reused components into their use files

--- lib/ui/pages/care_screen.dart
+++ lib/ui/pages/care_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:inkstep/ui/pages/care/advice_snippet.dart';
-import 'package:inkstep/ui/pages/care/bullet_element.dart';
+import 'package:inkstep/ui/components/horizontal_divider.dart';
 
-class BodyText extends StatelessWidget {
+class _BodyText extends StatelessWidget {
 
-  const BodyText(this.text, {Key key}) : super(key: key);
+  const _BodyText(this.text, {Key key}) : super(key: key);
 
   final String text;
 
@@ -14,163 +13,236 @@ class BodyText extends StatelessWidget {
   }
 }
 
+class _BulletElement extends StatelessWidget {
+  const _BulletElement(this.text);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 8.0),
+          child: Container(
+            height: 10.0,
+            width: 10.0,
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+        Flexible(
+          child: Text(
+            text,
+            style:  Theme.of(context).textTheme.body2,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _AdviceSnippet extends StatelessWidget {
+  const _AdviceSnippet({
+    Key key,
+    @required this.description,
+    @required this.children,
+    @required this.timeOffset,
+    this.timeDescription,
+    this.preCare = false,
+  }) : super(key: key);
+
+  final String description;
+  final Duration timeOffset;
+  final String timeDescription;
+  final bool preCare;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            description,
+            style: Theme.of(context).textTheme.title.copyWith(fontWeight: FontWeight.w500),
+          ),
+          HorizontalDivider(
+            thickness: 4,
+            percentage: 20,
+            color: Colors.white,
+            alignment: MainAxisAlignment.start,
+            padding: EdgeInsets.symmetric(vertical: 10),
+          ),
+          for (Widget adviceElem in children) adviceElem,
+        ],
+      ),
+    );
+  }
+}
+
 class CareScreen extends StatelessWidget {
   const CareScreen({Key key, @required this.bookedTime}) : super(key: key);
 
   final DateTime bookedTime;
 
-  static const AdviceSnippet expectation = AdviceSnippet(
+  static const _AdviceSnippet expectation = _AdviceSnippet(
     description: 'What to expect!',
     timeOffset: Duration(days: 7 * 7),
     preCare: true,
     children: <Widget>[
-      BodyText('New tattoos are raw, open wounds and will hurt a bit, '
+      _BodyText('New tattoos are raw, open wounds and will hurt a bit, '
           'about as much as a mild to moderate skin burn. This is normal and to be expected.'),
-      BodyText(''),
-      BulletElement(
+      _BodyText(''),
+      _BulletElement(
         'The tattoo area will be sore (like the muscles underneath have just been stretched',
       ),
-      BulletElement('You will experience redness'),
-      BulletElement('You might expereince some bruising (skin will be raised and bumpy)'),
-      BulletElement('You might feel a bit run down or tired'),
-      BodyText(''),
-      BodyText('All these symptoms will gradually subside over the first week and will '
+      _BulletElement('You will experience redness'),
+      _BulletElement('You might expereince some bruising (skin will be raised and bumpy)'),
+      _BulletElement('You might feel a bit run down or tired'),
+      _BodyText(''),
+      _BodyText('All these symptoms will gradually subside over the first week and will '
           'be totally gone after 2-4 weeks.'),
     ],
   );
 
   // www.jhaiho.com/tattoo-care/
-  static const List<AdviceSnippet> snippets = <AdviceSnippet>[
-    AdviceSnippet(
+  static const List<_AdviceSnippet> snippets = <_AdviceSnippet>[
+    _AdviceSnippet(
       description: 'Precare (The day before)',
       timeDescription: 'Week 1: Day 00',
       timeOffset: Duration(days: 1),
       preCare: true,
       children: <Widget>[
-        BodyText('Come sober'),
-        BodyText('Hygiene'), // Co// me freshly showered! Tattooing required good hygiene. ],
-        BodyText('Prep the tattoo spot'),
-        BodyText('What to wear'), // loose, comfortable clothing - think black!,
-        BodyText('Eat well and stay hydrated'), // blood glucose dropping, bring a snack, well rested
+        _BodyText('Come sober'),
+        _BodyText('Hygiene'), // Co// me freshly showered! Tattooing required good hygiene. ],
+        _BodyText('Prep the tattoo spot'),
+        _BodyText('What to wear'), // loose, comfortable clothing - think black!,
+        _BodyText('Eat well and stay hydrated'), // blood glucose dropping, bring a snack, well rested
       ],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       description: 'Tattoo Day: Getting ready!',
       timeDescription: 'T-24hrs',
       timeOffset: Duration(days: 1),
       preCare: true,
       children: <Widget>[
-        BodyText('Your appointment day is finally here! And with it, the usual hits play - '
+        _BodyText('Your appointment day is finally here! And with it, the usual hits play - '
             '"Do I prep the tattoo spot? Should I shave? Can I do a shot to calm my '
             'nerves before I get inked? Can I get there early? WHAT DO I WEAR?!"'
             'Pause the tunes - we\'ve got some answers for you!'),
-        BodyText('Getting to your appointmnet'), // Be on time! Leave the friends,
-        BodyText('Stay still'),
-        BodyText('Breaks'),
-        BodyText('Duration'),
-        BodyText('Tipping'),
+        _BodyText('Getting to your appointmnet'), // Be on time! Leave the friends,
+        _BodyText('Stay still'),
+        _BodyText('Breaks'),
+        _BodyText('Duration'),
+        _BodyText('Tipping'),
       ],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       timeDescription: 'Week 1: Day 01',
       description: 'For the first 2 hours',
       timeOffset: Duration(hours: 2),
       children: <Widget>[
-        BodyText('Your tattoo is going to be sore for the rest of day one. '
+        _BodyText('Your tattoo is going to be sore for the rest of day one. '
             'It might look a bit red and swollen and feel warm to the touch due to blood rushing'
             ' to the spot while it heals.'),
-        BodyText(''),
-        BulletElement('Be gentle with your freshly inked tattoo, especially once you unwrap it,'
+        _BodyText(''),
+        _BulletElement('Be gentle with your freshly inked tattoo, especially once you unwrap it,'
             ' and avoid touching your tattoo – or letting anyone else touch it!'),
-        BodyText(''),
-        BulletElement('The wrap is basically a temporary bandage. Leave it on for as long as '
+        _BodyText(''),
+        _BulletElement('The wrap is basically a temporary bandage. Leave it on for as long as '
             'directed by your artist – this can be anything from an hour to the whole day, '
             'sometimes even longer.'),
-        BulletElement('Cut through the wrap carefully using scissors instead of peeling it right '
+        _BulletElement('Cut through the wrap carefully using scissors instead of peeling it right '
             'off as this could pull out some ink that hasn’t settled yet, especially if you were '
             'given a cloth wrap which tends to stick to the skin.'),
-        BulletElement('Refrain from long and/or hot showers – opt for shorter showers in room '
+        _BulletElement('Refrain from long and/or hot showers – opt for shorter showers in room '
             'temperature water, and try to keep your tattoo from getting wet.'),
-        BulletElement('For up to 48 hours after getting inked, avoid all of these – sorry, '
+        _BulletElement('For up to 48 hours after getting inked, avoid all of these – sorry, '
             'you’re going to have to delay that freshly inked party you were planning on throwing!')
       ],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       timeDescription: 'Week 1: Day 02',
       description: 'Caring for a Sore and Itchy Tattoo',
       timeOffset: Duration(days: 2),
       children: <Widget>[
-        BodyText('You’re likely still going to feel sore on the tattoo area for a few days more, '
+        _BodyText('You’re likely still going to feel sore on the tattoo area for a few days more, '
             'up to a week (or slightly longer for larger or more detailed tattoos).'),
-        BodyText(''),
-        BulletElement('Cleanse and moisturize at least twice during the day and once at '
+        _BodyText(''),
+        _BulletElement('Cleanse and moisturize at least twice during the day and once at '
             'night before you sleep – that’s three times a day!'),
-        BodyText(''),
-        BulletElement('Your tattoo might also start to get itchy at this point. '
+        _BodyText(''),
+        _BulletElement('Your tattoo might also start to get itchy at this point. '
             'And what are we NOT going to do? That’s right, we WILL NOT scratch!'),
-        BulletElement('Wear loose, comfortable clothing in smooth fabrics.'),
-        BulletElement('Do not apply any sunscreen or heavy products until your '
+        _BulletElement('Wear loose, comfortable clothing in smooth fabrics.'),
+        _BulletElement('Do not apply any sunscreen or heavy products until your '
             'tattoo is fully healed. Keep it out of the sun and water as much as possible.'),
-        BulletElement('It’s going to be uncomfortable for at least a week, especially if the '
+        _BulletElement('It’s going to be uncomfortable for at least a week, especially if the '
             'tattoo is quite large or is placed in a spot that is hard to avoid sleeping on.')
       ],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       timeDescription: 'Week 1: Day 03',
       description: 'Scab Central!',
       timeOffset: Duration(days: 3),
       children: <Widget>[
-        BodyText('Do not itch skin'),
-        BodyText('Moisturise tattoo area 3 times a day'),
-        BodyText('Apply cream every 8 hours'),
+        _BodyText('Do not itch skin'),
+        _BodyText('Moisturise tattoo area 3 times a day'),
+        _BodyText('Apply cream every 8 hours'),
       ],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       timeDescription: 'Week 1: Day 4/5',
       description: 'More Scabbing!',
       timeOffset: Duration(days: 5),
       children: <Widget>[],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       timeDescription: 'Week 2',
       description: 'Dreaded Tattoo Itch!',
       timeOffset: Duration(days: 14),
       children: <Widget>[
-        BodyText('You may have heard about this stage already – an itchy tattoo during week 2!'),
-        BodyText(''),
-        BulletElement('Annoying enough just because you have to refrain from scratching, '
+        _BodyText('You may have heard about this stage already – an itchy tattoo during week 2!'),
+        _BodyText(''),
+        _BulletElement('Annoying enough just because you have to refrain from scratching, '
             'this stage is also hard because your tattoo is going to start peeling and '
             'flaking and won’t look its best.'),
-        BodyText(''),
-        BulletElement('The scabs are now fully formed and are starting to come off, '
+        _BodyText(''),
+        _BulletElement('The scabs are now fully formed and are starting to come off, '
             'which is what is causing the peeling, flaking, and itching.'),
-        BulletElement('And just like the previous 5 days, what are we not going to do? '
+        _BulletElement('And just like the previous 5 days, what are we not going to do? '
             'Scratch, rub, pick at, or pull off the peeling skin.'),
-        BulletElement('Keep the area very clean and well-moisturized (using a light lotion, '
+        _BulletElement('Keep the area very clean and well-moisturized (using a light lotion, '
             'preferably your recommended aftercare lotion, or alternatively a light oil '
             'such as baby oil).'),
-        BulletElement('Moisturize after every wash and once before bed.'),
+        _BulletElement('Moisturize after every wash and once before bed.'),
       ],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       timeDescription: 'Week 3',
       description: 'Final count down',
       timeOffset: Duration(days: 4 * 7),
       children: <Widget>[],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       timeDescription: 'Week 4',
       description: 'More healing',
       timeOffset: Duration(days: 5 * 7),
       children: <Widget>[],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       timeDescription: 'Week 5',
       description: 'More healing',
       timeOffset: Duration(days: 6 * 7),
       children: <Widget>[],
     ),
-    AdviceSnippet(
+    _AdviceSnippet(
       timeDescription: 'Week 6',
       description: 'Lifetime Tattoo Care!',
       timeOffset: Duration(days: 6 * 7 + 1),
@@ -185,7 +257,7 @@ class CareScreen extends StatelessWidget {
   Widget _adviceWidget(DateTime bookedTime) {
     final DateTime currTime = DateTime.now();
 
-    for (AdviceSnippet adviceDialog in snippets) {
+    for (_AdviceSnippet adviceDialog in snippets) {
       DateTime adviceDate;
 
       if (adviceDialog.preCare) {
@@ -210,7 +282,7 @@ class CareScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final AdviceSnippet advice = _adviceWidget(bookedTime);
+    final _AdviceSnippet advice = _adviceWidget(bookedTime);
 
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
